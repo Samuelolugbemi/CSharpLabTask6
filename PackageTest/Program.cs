043d336b4eb8260915527a809fff61ce71d17839
Olugbemi Samuel <olugbemisamuel600@gmail.com>
2020-11-04 09:44:32 +0100

    First commit

--- PackageTest/Program.cs
+++ PackageTest/Program.cs
@@ -10,11 +10,13 @@ namespace PackageTest
             TwoDayPackage twoDay = new TwoDayPackage("Jeremy", "Stutgartt", "Oyo State","Hamman", "Bronze","Ota", 6.0, 4000M, "Two Day Package");
             Console.WriteLine("\n Packages processed polymorphically...\n");
             Package[] packages = new Package[2] { overnight, twoDay };
-
+            decimal sum  = 0M;
             foreach (Package package in packages)
             {
+                sum = sum + package.CalculateCost();
                 Console.WriteLine($"{package}\nShipping Cost: {package.CalculateCost():C}\n");
             }
+            Console.WriteLine($"Total Shipping Cost = {sum:C}");
         }
     }
 }
